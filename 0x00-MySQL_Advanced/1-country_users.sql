1ea0d73d4015a394e79c497fc7cc39599f6b89a9
Benson Kamau <benkabenson@gmail.com>
2023-05-06 13:51:27 +0000

    task done

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- this script is creating a table called users.
+
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL,
+name VARCHAR(255),
+country ENUM('US','CO','TN') NOT NULL DEFAULT 'US',
+PRIMARY KEY (Id),
+UNIQUE (email)
+);
+
