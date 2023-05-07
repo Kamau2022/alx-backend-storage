cd2f333a41ad56235e8cf483ed0754aa0901ef79
Benson Kamau <benkabenson@gmail.com>
2023-05-07 18:50:01 +0000

    to be fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- this script lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, MAX(split) - MIN(formed) AS lifespan
+SELECT band_name,  ABS(split - formed) AS lifespan
 FROM metal_bands GROUP BY 1 ORDER BY 2 DESC;
