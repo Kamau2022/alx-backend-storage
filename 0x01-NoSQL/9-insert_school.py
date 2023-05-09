409bc1ba565d1ac14f0ca18b4e506683a856723c
Benson Kamau <benkabenson@gmail.com>
2023-05-09 21:01:40 +0000

    task done

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this module is about mongodb database
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """a function that inserts a new document
+       in a collection based on kwargs
+    """
+    newlist = [{**kwargs}]
+    insert_new = mongo_collection.insert_many(newlist)
+    return insert_new.inserted_ids
