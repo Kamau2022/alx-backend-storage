a8364c057587b09697d2fcbc4488ac73fbcad8fc
Benson Kamau <benkabenson@gmail.com>
2023-05-09 20:13:09 +0000

    to be fixed

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this is a module on mongodb database
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """a function that changes all topics of a school 
+       document based on the name
+    """
+    result = mongo_collection.update_many(
+    {name : name},
+    {'$set': {'topics' : topics}});
+    return result
