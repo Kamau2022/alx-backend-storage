8cd1896c4632f67af34f6cad933187403d0f9a3d
Benson Kamau <benkabenson@gmail.com>
2023-05-11 21:21:28 +0000

    task done

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- this script creates a function SafeDiv that divides the first by the second number
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv( a INT, b INT )
+RETURNS FLOAT DETERMINISTIC
+
+BEGIN
+    IF b = 0 THEN 
+	RETURN 0; 
+    ELSE RETURN a / b; 
+    END IF;
+
+END; $$
+
+DELIMITER ;
